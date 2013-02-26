a2caefebe81f4c8e05abc47b901dd08e5474a95c
fabioz <fabiofz@gmail.com>
2013-02-26 10:40:00 -0300

    Removed things we won't be using.

--- com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/ColorThemeManager.java
+++ com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/ColorThemeManager.java
@@ -1,8 +1,21 @@
 package com.github.eclipsecolortheme;
 
-import static com.github.eclipsecolortheme.ColorThemeKeys.*;
+import static com.github.eclipsecolortheme.ColorThemeKeys.BACKGROUND;
+import static com.github.eclipsecolortheme.ColorThemeKeys.CURRENT_LINE;
+import static com.github.eclipsecolortheme.ColorThemeKeys.DEBUG_CURRENT_INSTRUCTION_POINTER;
+import static com.github.eclipsecolortheme.ColorThemeKeys.DEBUG_SECONDARY_INSTRUCTION_POINTER;
+import static com.github.eclipsecolortheme.ColorThemeKeys.FIELD;
+import static com.github.eclipsecolortheme.ColorThemeKeys.FOREGROUND;
+import static com.github.eclipsecolortheme.ColorThemeKeys.JAVADOC;
+import static com.github.eclipsecolortheme.ColorThemeKeys.JAVADOC_KEYWORD;
+import static com.github.eclipsecolortheme.ColorThemeKeys.JAVADOC_LINK;
+import static com.github.eclipsecolortheme.ColorThemeKeys.JAVADOC_TAG;
+import static com.github.eclipsecolortheme.ColorThemeKeys.LOCAL_VARIABLE;
+import static com.github.eclipsecolortheme.ColorThemeKeys.METHOD;
+import static com.github.eclipsecolortheme.ColorThemeKeys.MULTI_LINE_COMMENT;
+import static com.github.eclipsecolortheme.ColorThemeKeys.OCCURRENCE_INDICATION;
+import static com.github.eclipsecolortheme.ColorThemeKeys.WRITE_OCCURRENCE_INDICATION;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
@@ -16,7 +29,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.osgi.framework.Bundle;
 import org.osgi.service.prefs.BackingStoreException;
 import org.w3c.dom.Document;
@@ -30,15 +42,15 @@ import com.github.eclipsecolortheme.mapper.ThemePreferenceMapper;
 
 /** Loads and applies color themes. */
 public class ColorThemeManager {
-	
+
 	private Map<String, ColorTheme> themes;
 	private Set<ThemePreferenceMapper> editors;
 
 	/** Creates a new color theme manager. */
 	public ColorThemeManager() {
 		themes = new HashMap<String, ColorTheme>();
-		readStockThemes(themes);
-		readImportedThemes(themes);
+		// readStockThemes(themes);
+		// readImportedThemes(themes);
 
 		editors = new HashSet<ThemePreferenceMapper>();
 		IConfigurationElement[] config = Platform.getExtensionRegistry()
@@ -68,56 +80,57 @@ public class ColorThemeManager {
 		}
 	}
 
-	private static void readStockThemes(Map<String, ColorTheme> themes) {
-		IConfigurationElement[] config = Platform.getExtensionRegistry()
-				.getConfigurationElementsFor(
-						Activator.EXTENSION_POINT_ID_THEME);
-		try {
-			for (IConfigurationElement e : config) {
-				String xml = e.getAttribute("file");
-				String contributorPluginId = e.getContributor().getName();
-				Bundle bundle = Platform.getBundle(contributorPluginId);
-				InputStream input = (InputStream) bundle.getResource(xml)
-						.getContent();
-				ColorTheme theme = parseTheme(input);
-				amendThemeEntries(theme.getEntries());
-				themes.put(theme.getName(), theme);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	// We won't be using stock themes
+	// private static void readStockThemes(Map<String, ColorTheme> themes) {
+	// IConfigurationElement[] config = Platform.getExtensionRegistry()
+	// .getConfigurationElementsFor(
+	// Activator.EXTENSION_POINT_ID_THEME);
+	// try {
+	// for (IConfigurationElement e : config) {
+	// String xml = e.getAttribute("file");
+	// String contributorPluginId = e.getContributor().getName();
+	// Bundle bundle = Platform.getBundle(contributorPluginId);
+	// InputStream input = (InputStream) bundle.getResource(xml)
+	// .getContent();
+	// ColorTheme theme = parseTheme(input);
+	// amendThemeEntries(theme.getEntries());
+	// themes.put(theme.getName(), theme);
+	// }
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
 
-	private static void readImportedThemes(Map<String, ColorTheme> themes) {
-		IPreferenceStore store = getPreferenceStore();
+	// private static void readImportedThemes(Map<String, ColorTheme> themes) {
+	// IPreferenceStore store = getPreferenceStore();
+	//
+	// for (int i = 1;; i++) {
+	// String xml = store.getString("importedColorTheme" + i);
+	// if (xml == null || xml.length() == 0)
+	// break;
+	// try {
+	// ColorTheme theme = parseTheme(new ByteArrayInputStream(
+	// xml.getBytes()));
+	// amendThemeEntries(theme.getEntries());
+	// themes.put(theme.getName(), theme);
+	// } catch (Exception e) {
+	// System.err.println("Error while parsing imported theme");
+	// e.printStackTrace();
+	// }
+	// }
+	// }
 
-		for (int i = 1;; i++) {
-			String xml = store.getString("importedColorTheme" + i);
-			if (xml == null || xml.length() == 0)
-				break;
-			try {
-				ColorTheme theme = parseTheme(new ByteArrayInputStream(
-						xml.getBytes()));
-				amendThemeEntries(theme.getEntries());
-				themes.put(theme.getName(), theme);
-			} catch (Exception e) {
-				System.err.println("Error while parsing imported theme");
-				e.printStackTrace();
-			}
-		}
-	}
+	// public void clearImportedThemes() {
+	// IPreferenceStore store = getPreferenceStore();
+	// for (int i = 1; store.contains("importedColorTheme" + i); i++)
+	// store.setToDefault("importedColorTheme" + i);
+	// themes.clear();
+	// readStockThemes(themes);
+	// }
 
-	public void clearImportedThemes() {
-		IPreferenceStore store = getPreferenceStore();
-		for (int i = 1; store.contains("importedColorTheme" + i); i++)
-			store.setToDefault("importedColorTheme" + i);
-		themes.clear();
-		readStockThemes(themes);
-	}
-
-	private static IPreferenceStore getPreferenceStore() {
-		return Activator.getDefault().getPreferenceStore();
-	}
+	// private static IPreferenceStore getPreferenceStore() {
+	// return Activator.getDefault().getPreferenceStore();
+	// }
 
 	public static ColorTheme parseTheme(InputStream input)
 			throws ParserConfigurationException, SAXException, IOException {
@@ -161,6 +174,8 @@ public class ColorThemeManager {
 				entries.put(entryNode.getNodeName(), setting);
 			}
 		}
+		// Fabioz Change: amend entries before setting them
+		amendThemeEntries(entries);
 		theme.setEntries(entries);
 
 		return theme;
@@ -186,33 +201,41 @@ public class ColorThemeManager {
 			theme.put(key, theme.get(defaultKey));
 	}
 
-	/**
-	 * Returns all available color themes.
-	 * @return all available color themes.
-	 */
-	public Set<ColorTheme> getThemes() {
-		return new HashSet<ColorTheme>(themes.values());
-	}
-
-	/**
-	 * Returns the theme stored under the supplied name. 
-	 * @param name The name of the theme.
-	 * @return The requested theme or <code>null</code> if none was stored under
-	 *         the supplied name.
-	 */
-    public ColorTheme getTheme(String name) {
-        return themes.get(name);
-    }
+	// /**
+	// * Returns all available color themes.
+	// *
+	// * @return all available color themes.
+	// */
+	// public Set<ColorTheme> getThemes() {
+	// return new HashSet<ColorTheme>(themes.values());
+	// }
+	//
+	// /**
+	// * Returns the theme stored under the supplied name.
+	// *
+	// * @param name
+	// * The name of the theme.
+	// * @return The requested theme or <code>null</code> if none was stored
+	// under
+	// * the supplied name.
+	// */
+	// public ColorTheme getTheme(String name) {
+	// return themes.get(name);
+	// }
 
 	/**
 	 * Changes the preferences of other plugins to apply the color theme.
-	 * @param theme The name of the color theme to apply.
+	 * 
+	 * @param theme
+	 *            the theme to be applied. If null will just remove the colors
+	 *            set and restore the defaults.
+	 * 
 	 */
-	public void applyTheme(String theme) {
+	public void applyTheme(ColorTheme theme) {
 		for (ThemePreferenceMapper editor : editors) {
 			editor.clear();
-			if (themes.get(theme) != null)
-				editor.map(themes.get(theme).getEntries());
+			if (theme != null)
+				editor.map(theme.getEntries());
 
 			try {
 				editor.flush();
@@ -223,29 +246,32 @@ public class ColorThemeManager {
 		}
 	}
 
-	/**
-	 * Adds the color theme to the list and saves it to the preferences.
-	 * Existing themes will be overwritten with the new content.
-	 * @param content The content of the color theme file.
-	 * @return The saved color theme, or <code>null</code> if the theme was not
-	 *         valid.
-	 */
-	public ColorTheme saveTheme(String content) {
-		ColorTheme theme;
-		try {
-			theme = ColorThemeManager.parseTheme(new ByteArrayInputStream(
-					content.getBytes()));
-			String name = theme.getName();
-			themes.put(name, theme);
-			IPreferenceStore store = getPreferenceStore();
-			for (int i = 1;; i++)
-				if (!store.contains("importedColorTheme" + i)) {
-					store.putValue("importedColorTheme" + i, content);
-					break;
-				}
-			return theme;
-		} catch (Exception e) {
-			return null;
-		}
-	}
+	// /**
+	// * Adds the color theme to the list and saves it to the preferences.
+	// * Existing themes will be overwritten with the new content.
+	// *
+	// * @param content
+	// * The content of the color theme file.
+	// * @return The saved color theme, or <code>null</code> if the theme was
+	// not
+	// * valid.
+	// */
+	// public ColorTheme saveTheme(String content) {
+	// ColorTheme theme;
+	// try {
+	// theme = ColorThemeManager.parseTheme(new ByteArrayInputStream(
+	// content.getBytes()));
+	// String name = theme.getName();
+	// themes.put(name, theme);
+	// IPreferenceStore store = getPreferenceStore();
+	// for (int i = 1;; i++)
+	// if (!store.contains("importedColorTheme" + i)) {
+	// store.putValue("importedColorTheme" + i, content);
+	// break;
+	// }
+	// return theme;
+	// } catch (Exception e) {
+	// return null;
+	// }
+	// }
 }
--- com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/preferences/PreferenceInitializer.java
+++ com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/preferences/PreferenceInitializer.java
@@ -1,15 +1,15 @@
-package com.github.eclipsecolortheme.preferences;
-
-import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.jface.preference.IPreferenceStore;
-
-import com.github.eclipsecolortheme.Activator;
-
-/** Initialises this plugin's preferences. */
-public class PreferenceInitializer extends AbstractPreferenceInitializer {
-    @Override
-    public void initializeDefaultPreferences() {
-        IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-        store.setDefault("colorTheme", "default");
-    }
-}
+//package com.github.eclipsecolortheme.preferences;
+//
+//import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+//import org.eclipse.jface.preference.IPreferenceStore;
+//
+//import com.github.eclipsecolortheme.Activator;
+//
+///** Initialises this plugin's preferences. */
+//public class PreferenceInitializer extends AbstractPreferenceInitializer {
+//    @Override
+//    public void initializeDefaultPreferences() {
+//        IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+//        store.setDefault("colorTheme", "default");
+//    }
+// }
