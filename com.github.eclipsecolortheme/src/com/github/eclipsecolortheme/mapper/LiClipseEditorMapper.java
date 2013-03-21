466f387b4d5652c8f66d0523a0509858f196f9f3
fabioz <fabiofz@gmail.com>
2013-03-21 17:34:35 -0300

    Adding some mappings to experiment with liclipse.

---
+++ com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/mapper/LiClipseEditorMapper.java
@@ -0,0 +1,35 @@
+package com.github.eclipsecolortheme.mapper;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import com.github.eclipsecolortheme.ColorThemeMapping;
+import com.github.eclipsecolortheme.ColorThemeSetting;
+
+public class LiClipseEditorMapper extends GenericMapper {
+	private class Mapping extends ColorThemeMapping {
+		public Mapping(String pluginKey, String themeKey) {
+			super(pluginKey, themeKey);
+		}
+
+		@Override
+		public void putPreferences(IEclipsePreferences preferences,
+				ColorThemeSetting setting) {
+			preferences.put(pluginKey + "_COLOR", setting.getColor().asRGB());
+
+			String styleKey = pluginKey + "_STYLE";
+			int styleVal = 0;
+			if (setting.isBoldEnabled() != null && setting.isBoldEnabled()) {
+				styleVal += 1;
+			}
+			if (setting.isItalicEnabled() != null && setting.isItalicEnabled()) {
+				styleVal += 2;
+			}
+			preferences.putInt(styleKey, styleVal);
+		}
+	}
+
+	@Override
+	protected ColorThemeMapping createMapping(String pluginKey, String themeKey) {
+		return new Mapping(pluginKey, themeKey);
+	}
+}
